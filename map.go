b1191c67ac1d6545640b22f0765b95982d64ab06
chen <chen.su1994@outlook.com>
2023-08-07 21:09:24 +0800

    add more funcs for map

--- map.go
+++ map.go
@@ -5,7 +5,7 @@ package gfn
 // map1 := map[int]struct{}{1: {}, 2: {}, 3: {}}
 // map2 := map[int]struct{}{1: {}, 2: {}, 3: {}}
 // Compare(map1, map2) // true
-func Compare[T, V comparable](a map[T]V, b map[T]V) bool {
+func Compare[K, V comparable](a map[K]V, b map[K]V) bool {
 	if len(a) != len(b) {
 		return false
 	}
@@ -16,4 +16,155 @@ func Compare[T, V comparable](a map[T]V, b map[T]V) bool {
 		}
 	}
 	return true
+}
+
+// Keys returns the keys of a map.
+// @example
+// gfn.Keys(map[int]string{1: "a", 2: "b", 3: "c"})
+// // []int{1, 2, 3} or []int{3, 2, 1} or []int{2, 1, 3} etc.
+func Keys[K comparable, V any](m map[K]V) []K {
+	keys := make([]K, len(m))
+	i := 0
+	for k := range m {
+		keys[i] = k
+		i++
+	}
+	return keys
+}
+
+// Values returns the values of a map.
+// @example
+// gfn.Values(map[int]string{1: "a", 2: "b", 3: "c"})
+// // []string{"a", "b", "c"} or []string{"c", "b", "a"} or []string{"b", "a", "c"} etc.
+func Values[K comparable, V any](m map[K]V) []V {
+	values := make([]V, len(m))
+	i := 0
+	for _, v := range m {
+		values[i] = v
+		i++
+	}
+	return values
+}
+
+/* @example Invert
+m := map[string]string{
+	"Array": "array.go",
+	"Map":   "map.go",
+	"Set":   "set.go",
+	"Math":  "math.go",
+}
+
+gfn.Invert(m)
+// map[string]string{
+// 	"array.go": "Array",
+// 	"map.go":   "Map",
+// 	"set.go":   "Set",
+// 	"math.go":  "Math",
+// }
+*/
+
+// Invert returns a map with keys and values swapped.
+func Invert[K, V comparable](m map[K]V) map[V]K {
+	res := make(map[V]K)
+	for k, v := range m {
+		res[v] = k
+	}
+	return res
+}
+
+// Clear removes all keys from a map.
+// @example
+// m := map[int]string{1: "a", 2: "b", 3: "c"}
+// gfn.Clear(m)
+// // m is now an empty map
+func Clear[K comparable, V any](m map[K]V) {
+	for k := range m {
+		delete(m, k)
+	}
+}
+
+/* @example Items
+m := map[int]string{1: "a", 2: "b", 3: "c"}
+
+gfn.Items(m)
+// []Pair[int, string]{
+// 	{1, "a"},
+// 	{2, "b"},
+// 	{3, "c"},
+// }
+*/
+
+// Items returns a slice of pairs of keys and values.
+func Items[K comparable, V any](m map[K]V) []Pair[K, V] {
+	items := make([]Pair[K, V], len(m))
+	i := 0
+	for k, v := range m {
+		items[i] = Pair[K, V]{k, v}
+		i++
+	}
+	return items
+}
+
+// Update updates a map with the keys and values from other maps.
+// @example
+// m1 := map[int]string{1: "a", 2: "b", 3: "c"}
+// m2 := map[int]string{1: "d", 2: "e"}
+// m3 := map[int]string{1: "f"}
+// Update(m1, m2, m3)
+// // map[int]string{1: "f", 2: "e", 3: "c"}
+func Update[K comparable, V any](m map[K]V, other ...map[K]V) {
+	for _, o := range other {
+		for k, v := range o {
+			m[k] = v
+		}
+	}
+}
+
+// Clone returns a shallow copy of a map.
+// @example
+// m := map[int]string{1: "a", 2: "b", 3: "c"}
+// m2 := Clone(m)
+// // m2 is a copy of m
+func Clone[K comparable, V any](m map[K]V) map[K]V {
+	res := make(map[K]V, len(m))
+	for k, v := range m {
+		res[k] = v
+	}
+	return res
+}
+
+/* @example DeleteBy
+m := map[int]string{1: "a", 2: "b", 3: "c"}
+DeleteBy(m, func(k int, v string) bool {
+	return k == 1 || v == "c"
+})
+// map[int]string{2: "b"}
+*/
+
+// DeleteBy deletes keys from a map if the predicate function returns true.
+func DeleteBy[K comparable, V any](m map[K]V, deleteFn func(K, V) bool) {
+	for k, v := range m {
+		if deleteFn(k, v) {
+			delete(m, k)
+		}
+	}
+}
+
+/* @example Rejected
+m := map[int]string{1: "a", 2: "b", 3: "c"}
+rejected := Rejected(m, func(k int, v string) bool {
+	return k == 1 || v == "c"
+})
+// map[int]string{2: "b"}
+*/
+
+// Rejected returns a map with keys and values that don't pass the predicate function.
+func Rejected[K comparable, V any](m map[K]V, keep func(K, V) bool) map[K]V {
+	res := make(map[K]V)
+	for k, v := range m {
+		if !keep(k, v) {
+			res[k] = v
+		}
+	}
+	return res
 }
--- map_test.go
+++ map_test.go
@@ -1,19 +1,22 @@
 package gfn_test
 
 import (
+	"sort"
 	"strconv"
 	"testing"
 
 	. "github.com/suchen-sci/gfn"
 )
 
-func TestSame(t *testing.T) {
+func TestCompare(t *testing.T) {
 	{
-		map1 := map[int]struct{}{}
-		map2 := map[int]struct{}{}
+		type Number int
+		type Numbers map[Number]struct{}
+		map1 := Numbers{}
+		map2 := Numbers{}
 		for i := 0; i < 100; i++ {
-			map1[i] = struct{}{}
-			map2[i] = struct{}{}
+			map1[Number(i)] = struct{}{}
+			map2[Number(i)] = struct{}{}
 		}
 		AssertTrue(t, Compare(map1, map2))
 
@@ -33,4 +36,115 @@ func TestSame(t *testing.T) {
 		map2[0] = "999"
 		AssertFalse(t, Compare(map1, map2))
 	}
+}
+
+func TestKeys(t *testing.T) {
+	{
+		keys := Keys(map[int]string{1: "a", 2: "b", 3: "c"})
+		sort.Ints(keys)
+		AssertSliceEqual(t, []int{1, 2, 3}, keys)
+	}
+
+	{
+		type Number int
+		expected := []Number{1, 2, 3}
+		keys := Keys(map[Number]string{1: "a", 2: "b", 3: "c"})
+		sort.Slice(keys, func(i, j int) bool {
+			return keys[i] < keys[j]
+		})
+		AssertSliceEqual(t, expected, keys)
+	}
+}
+
+func TestValues(t *testing.T) {
+	values := Values(map[int]string{1: "a", 2: "b", 3: "c"})
+	sort.Strings(values)
+	AssertSliceEqual(t, []string{"a", "b", "c"}, values)
+}
+
+func TestInvert(t *testing.T) {
+	{
+		m := map[string]string{
+			"Array": "array.go",
+			"Map":   "map.go",
+			"Set":   "set.go",
+			"Math":  "math.go",
+		}
+		expected := map[string]string{
+			"array.go": "Array",
+			"map.go":   "Map",
+			"set.go":   "Set",
+			"math.go":  "Math",
+		}
+		AssertTrue(t, Compare(expected, Invert(m)))
+	}
+
+	{
+		m := map[string]int{
+			"a": 1,
+			"b": 2,
+			"c": 3,
+		}
+		expected := map[int]string{
+			1: "a",
+			2: "b",
+			3: "c",
+		}
+		AssertTrue(t, Compare(expected, Invert(m)))
+	}
+}
+
+func TestClear(t *testing.T) {
+	m := map[int]float32{}
+	for i := 0; i < 1000; i++ {
+		m[i] = float32(i)
+	}
+	Clear(m)
+	AssertEqual(t, 0, len(m))
+}
+
+func TestItems(t *testing.T) {
+	items := Items(map[int]string{1: "a", 2: "b", 3: "c"})
+	sort.Slice(items, func(i, j int) bool {
+		return items[i].First < items[j].First
+	})
+	expected := []Pair[int, string]{
+		{1, "a"},
+		{2, "b"},
+		{3, "c"},
+	}
+	AssertSliceEqual(t, expected, items)
+}
+
+func TestUpdate(t *testing.T) {
+	m1 := map[int]string{1: "a", 2: "b", 3: "c"}
+	m2 := map[int]string{1: "d", 2: "e"}
+	m3 := map[int]string{1: "f"}
+	expected := map[int]string{1: "f", 2: "e", 3: "c"}
+	Update(m1, m2, m3)
+	AssertTrue(t, Compare(expected, m1))
+}
+
+func TestClone(t *testing.T) {
+	m := map[int]string{1: "a", 2: "b", 3: "c"}
+	clone := Clone(m)
+	AssertTrue(t, Compare(m, clone))
+	m[1] = "d"
+	AssertFalse(t, Compare(m, clone))
+}
+
+func TestDeleteBy(t *testing.T) {
+	m := map[int]string{1: "a", 2: "b", 3: "c"}
+	DeleteBy(m, func(k int, v string) bool {
+		return k == 1 || v == "c"
+	})
+	AssertTrue(t, Compare(map[int]string{2: "b"}, m))
+}
+
+func TestRejected(t *testing.T) {
+	m := map[int]string{1: "a", 2: "b", 3: "c"}
+	rejected := Rejected(m, func(k int, v string) bool {
+		return k == 1 || v == "c"
+	})
+	AssertTrue(t, Compare(map[int]string{2: "b"}, rejected))
 }
